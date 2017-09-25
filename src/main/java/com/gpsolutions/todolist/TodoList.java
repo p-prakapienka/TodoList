5c7fe364b88529ff6bb256396dae3e465e00ea53
Paviel Prakapienka <P.Prokopenko@gpsolutions.com>
2017-09-25 19:18:59 +0300

    0.0.2 switch to war

--- src/main/java/com/gpsolutions/todolist/TodoList.java
+++ src/main/java/com/gpsolutions/todolist/TodoList.java
@@ -2,9 +2,16 @@ package com.gpsolutions.todolist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoList {
+public class TodoList extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TodoList.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TodoList.class);
