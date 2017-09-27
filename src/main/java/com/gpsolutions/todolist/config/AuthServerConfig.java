227263082dd754641e50b847d5c233231518728b
Paviel Prakapienka <P.Prokopenko@gpsolutions.com>
2017-09-27 16:30:55 +0300

    0.1.0 Start working on oauth

---
+++ src/main/java/com/gpsolutions/todolist/config/AuthServerConfig.java
@@ -0,0 +1,42 @@
+package com.gpsolutions.todolist.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    @Qualifier("authenticationManagerBean")
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients
+            .inMemory()
+            .withClient("clientId")
+            .authorizedGrantTypes("password", "refresh-token")
+            .accessTokenValiditySeconds(120)
+            .scopes("read", "write")
+            .secret("secret")
+            .resourceIds("bebe");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+            .authenticationManager(authenticationManager)
+            .userDetailsService(userDetailsService);
+    }
+}
--- src/main/java/com/gpsolutions/todolist/config/SecurityConfig.java
+++ src/main/java/com/gpsolutions/todolist/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.gpsolutions.todolist.config;
 
-import static com.gpsolutions.todolist.model.User.ROLE_ADMIN;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -20,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
-    @Override
+    /*@Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
@@ -30,19 +30,31 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .httpBasic()
             .and()
             .csrf().disable();
-    }
+    }*/
 
-    @Autowired
+    /*@Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers()
+    }*/
+
+    /*@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
             //.inMemoryAuthentication()
             //.withUser("user").password("password").roles("ADMIN");
             .userDetailsService(userDetailsService).passwordEncoder(encoder());
-    }
+    }*/
 
     @Bean
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean()
+        throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
